741c16a73f13a1129f8104c46289e42addc9297b
Yasuaki NIWANO <cloud.yasuaki.enzoferrari@gmail.com>
2020-05-27 21:12:45 +0900

    プロジェクト ファイルを追加します。

---
+++ CMAES.NET/CMA.cs
@@ -0,0 +1,18 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+
+namespace CMAES.NET
+{
+    public class CMA
+    {
+        public int Dim { get; private set; }
+        public int PopulationSize { get; private set; }
+        public int Generation { get; set; }
+
+        public CMA(Vector<double> mean, double sigma, Vector<double> bounds = null, int n_max_resampling = 100, int seed = 0)
+        {
+            if (!(sigma > 0))
+                throw new ArgumentOutOfRangeException("sigma must be non-zero positive value");
+        }
+    }
+}
